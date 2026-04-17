a0a4aab25ce3823540e65b35899b3e8cef752fcc
spyzz-hash <surekhaabi@gmail.com>
2026-04-17 23:04:01 +0530

    Add Vercel Postgres support

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -74,14 +74,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chatbot_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        env='POSTGRES_URL',
+        conn_max_age=600,
+    )
 }
 
 
